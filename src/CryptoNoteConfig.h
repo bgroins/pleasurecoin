0f5d489693d6e3cb7dac832ca01c1f5178e206ec
exo3000 <exobyte@protonmail.com>
2017-06-04 00:05:31 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -74,7 +74,7 @@ const char     MINER_CONFIG_FILE_NAME[]                      = "miner_conf.json"
 
 //TODO Put here the name of your currency
 const char     CRYPTONOTE_NAME[]                             = "PleasureCoin";
-const char     GENESIS_COINBASE_TX_HEX[]                     = "013c01ff0001f3bdbcc189f202029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012895784b70f2ad33a06ab0e0f23bd574c330e55a62065193edaded57a518197c";
+const char     GENESIS_COINBASE_TX_HEX[]                     = "";
 
 const uint8_t  CURRENT_TRANSACTION_VERSION                   =  1;
 const uint8_t  BLOCK_MAJOR_VERSION_1                         =  1;
